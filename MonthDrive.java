21c0bbf226fe2254822c621e1b3639cda766c746
edwardsz8437 <51128155+edwardsz8437@users.noreply.github.com>
2019-06-05 10:36:51 -0700

    Add files via upload

--- MonthDrive.java
+++ MonthDrive.java
@@ -1,14 +1,15 @@
 
 public class MonthDrive {
 public static void main(String[] args)
-{   Day first=new Day("May", 1, 2019, 5, 0, 0);
-Day second=new Day("May", 2, 2019, 0, 0, 0);
-Day third=new Day("May", 3, 2019, 0, 0, 0);
+{   Day first=new Day("May", 1, 2019, 5, 10, 0);
+Day second=new Day("May", 2, 2019, 0, 10, 0);
+Day third=new Day("May", 3, 2019, 0, 10, 0);
 
 	
 	Month May=new Month(first, second, third);
 
-	System.out.print("Average mood: " + May.findAverageMood());
-	
+	System.out.print("Average mood: " + May.findAverageMood() + "\n");
+	System.out.print("Average Sleep: " + May.findAverageSleep()+"\n");
+	System.out.print("Average spending: $" + May.findAverageFinances()+"\n");
 }
 }
