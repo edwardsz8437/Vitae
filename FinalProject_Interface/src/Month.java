85afe6f26172eb6cd8182d20bfe1fc0be2e77eed
Piggygeek <piggygeek@gmail.com>
2019-06-07 08:28:16 -0700

    fixed month

--- FinalProject_Interface/src/Month.java
+++ FinalProject_Interface/src/Month.java
@@ -24,14 +24,12 @@ public class Month {
 	Day third=new Day("May", 3, 2019, 0, 0, 0);
 	
 	public double findAverageMood()
-	{
-		
-		
+	{		
 		int totalMood = 0;
 		double average = 0;
 		
 
-		for (int i=0;i<days.size();i++)
+		for (int i=0;i<days.size();i++) //adds all the mood levels together from the month 
 		{
 			
 			totalMood+=days.get(i).getMoodLevel();
@@ -41,5 +39,82 @@ public class Month {
 		average=(double)totalMood/days.size();
 		return average;
 	}
+	
+	
+	public String badSleepEffect()
+	{
+		String badSleep[]=new String[]{"memory issues", "mood changes", "high blood pressure", "increased risk of diabetes", "weight gain",
+		"weakened immune system"};
+		
+		return badSleep[(int)(Math.random())*6];
+		
+		
+	}
+	
+	
+	public String goodSleepEffect()
+	{
+		String goodSleep[]=new String[]{"happy mood", "increased memory", "strong immune system", "decreased risk of disease", "alertness",
+		"more energy"};
+		
+		return goodSleep[(int)(Math.random())*6];
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	public String findAverageSleep()
+	{
+		double totalSleep = 0;
+		double average = 0;
+		String statement = "";
+
+		for (int i=0;i<days.size();i++) //adds all the sleep levels together from the month
+		{
+			
+		totalSleep=totalSleep + days.get(i).getSleepAmount();
+			
+		}
+		
+		average=(double)totalSleep/days.size();
+		
+		if (average<6)
+		{
+			statement=average + "\n" +"You should be getting more sleep." + "\n" + " A side effect of sleep deprivation is: "+ badSleepEffect();
+		}
+		
+		
+		if (average>6)
+		{
+			statement=average + "\n"+ "You are getting a good amount of sleep." + "\n" + " Good sleep leads to: "+goodSleepEffect();
+		}
+		
+		return statement;
+		
+	}
+	
+	public double findAverageFinances()
+	{
+		double totalFinances = 0;
+		double average = 0;
+		   
+
+		for (int i=0;i<days.size();i++) //adds all the sleep levels together from the month
+		{
+			
+			totalFinances=totalFinances + days.get(i).getSleepAmount();
+			
+		}
+		
+		average=(double)totalFinances/days.size();
+		return average;
+		
+	}
+
 
 }
